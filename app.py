84b58bbd263b01e266f767982391ace5bbb54ac4
marius nastase <mnastase19@yahoo.com>
2022-12-23 23:34:47 +0200

    implemented login and register endpoints + add dashboard html page
    Signed-off-by: marius nastase <mnastase19@yahoo.com>

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
+from bson import ObjectId
 from flask import Flask, render_template, url_for, redirect
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 import db
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-import json
-from json import JSONEncoder
+import PySimpleGUI as sg
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 bcrypt = Bcrypt(app)
 
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
-class UserEncoder(json.JSONEncoder):
-    def default(self, o):
-            return o.__dict__
+@login_manager.user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    users = db.db.user
+    user_json = users.find_one({'_id': ObjectId(user_id)})
+    return User(user_json)
+
+class User(UserMixin):
+    def __init__(self, user_json):
+        self.user_json = user_json
+
+    # Overriding get_id is required if you don't have the id property
+    # Check the source code for UserMixin for details
+    def get_id(self):
+        object_id = self.user_json.get('_id')
+        return str(object_id)
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
@@ -31,11 +43,6 @@ class RegisterForm(FlaskForm):
 
     submit = SubmitField('Register')
 
-    def validate_username(self, username):
-        existing_user_name = getUser(username)
-        if existing_user_name:
-            return True
-        return False
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[
@@ -46,9 +53,9 @@ class LoginForm(FlaskForm):
 
     submit = SubmitField('Login')
 
-@app.route('/users/username')
+@app.route('/user/')
 def getUser(newUsername):
-    return db.db.user.find({'username' : newUsername})
+    return db.db.user.find_one({'username' : newUsername})
 
 @app.route('/')
 def home():
@@ -58,29 +65,38 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = getUser(form.username.data)
+        if user:
+            if bcrypt.check_password_hash(user['password'], form.password.data):
+                loginuser = User(user)
+                login_user(loginuser)
+                return redirect(url_for('dashboard'))
+            else:
+                sg.Popup('Oops', 'Wrong password! Try again!')
     return render_template('login.html', form = form)
 
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
 
-    hashed_password = bcrypt.generate_password_hash(form.password.data)
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        existing_user_name = User(getUser(form.username.data))
+        if existing_user_name:
+            sg.Popup('Username already exists!')
 
-    if form.validate_username(form.username.data):
-        raise ValidationError(
-            'That username already exists. Please choose a different one.')
-
-    db.db.user.insert_one({'username':form.username.data, 'password':hashed_password})
-    redirect(url_for('login'))
+        else:
+            db.db.user.insert_one({'username': form.username.data, 'password': hashed_password})
+            return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
-
-
-@app.route('/test')
-def test():
-    db.db.collection.insert_one({"name": "John"})
-    return "Connected to the data base!"
 
 
 if __name__ == '__main__':
