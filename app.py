dbc123bb98dcd21589c3c922ec3cb3cf7e8b63ec
marius nastase <mnastase19@yahoo.com>
2022-12-23 12:47:57 +0200

    added forms cells in html
    Signed-off-by: marius nastase <mnastase19@yahoo.com>

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_login import UserMixin
 import db
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+import json
+from json import JSONEncoder
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+bcrypt = Bcrypt(app)
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class UserEncoder(json.JSONEncoder):
+    def default(self, o):
+            return o.__dict__
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
@@ -17,26 +32,49 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        existing_user_name = db.db.getUser(username)
-        return
+        existing_user_name = getUser(username)
+        if existing_user_name:
+            return True
+        return False
 
-@app.route('/users/usrname')
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+@app.route('/users/username')
 def getUser(newUsername):
-    return
+    return db.db.user.find({'username' : newUsername})
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form = form)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+
+    hashed_password = bcrypt.generate_password_hash(form.password.data)
+
+    if form.validate_username(form.username.data):
+        raise ValidationError(
+            'That username already exists. Please choose a different one.')
+
+    db.db.user.insert_one({'username':form.username.data, 'password':hashed_password})
+    redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 
 @app.route('/test')
@@ -50,9 +88,4 @@ if __name__ == '__main__':
         db.db.create_collection("user")
     except:
         pass
-    try:
-        user = {"username": "john", "password": "pass"}
-        db.db.user.insertOne(user)
-    except:
-        print("eroare")
     app.run(debug=True, port=8000)
